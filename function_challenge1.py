04b29eb51208c7dbd4efb816b87d4c65f230bfec
worladev <confidenceworlasi@gmail.com>
2023-06-18 01:58:39 +0100

    updated with solved questions

--- function_challenge1.py
+++ function_challenge1.py
@@ -1,5 +1,7 @@
-'''
-NO 1
+from collections import Counter
+from string import punctuation
+
+'''NO 1
 Write a function called matches that takes two strings as arguments and returns how many matches there
 are between the strings. 
 A match is where the two strings have the same character at the same index. For instance, 
@@ -16,8 +18,7 @@ def matches(str1, str2):
 print(matches("python", "path"))
 
 
-'''
-NO 2
+'''NO 2
 Write a function called one_away that takes two strings and returns True
 if the strings are of the same length and differ in exactly one letter,
 like bike/hike or water/wafer.
@@ -35,8 +36,7 @@ def one_way(str1, str2):
 print(one_way("bike", "hike"))
 
 
-'''
-NO 3
+'''NO 3
 - Write a function called first_diff that is given two strings and returns the first
 location in which the strings differ.
 If the strings are identical, it should return -1.
@@ -52,3 +52,54 @@ def first_diff(str1, str2):
 
 print(first_diff("hoke", "hike"))
 
+
+'''NO 4
+Implement a function called duplicate_count that takes a
+string as input and returns the count of characters that
+appear more than once in the string. The count should be
+case-insensitive.
+'''
+# SOLUTION 1
+def duplicate_count(str):
+    for punc in punctuation:
+        word = str.replace(punc, "")
+    
+    char_count = Counter(word)
+
+    count = 0
+    for value in char_count.values():
+        if value > 1:
+            count += 1
+    return count
+
+print(duplicate_count("function"))
+print(duplicate_count("This is a sentence string."))
+
+
+
+
+
+#SOLUTION 2
+def duplicate_count(str):
+    str = str.lower()  # Convert string to lowercase
+    char_count = {}  # Dictionary to store character counts
+
+#   Count the occurrences of each character in the string
+    for char in str:
+        char_count[char] = char_count.get(char, 0) + 1
+        # print(char_count)
+
+
+#   Counting the characters that appear more than once    
+    count = 0
+    # for key, value in char_count.items():
+    #     if key > 1:
+    #         count += 1
+
+    for value in char_count.values():
+        if value > 1:
+            count += 1
+    return count
+
+print(duplicate_count("functions now"))
+print(duplicate_count("This is a sentence string."))
