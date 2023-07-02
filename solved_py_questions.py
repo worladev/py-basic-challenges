d98ca252fd162dfa211e73846b4530484b9b515a
worladev <confidenceworlasi@gmail.com>
2023-07-02 20:40:28 +0100

    solved 7th question

--- solved_py_questions.py
+++ solved_py_questions.py
@@ -94,7 +94,35 @@ elif  unit == "Fahrenheit" or unit == "fahrenheit" or unit == "F" or unit == "f"
 
 
 
+'''NO 7
+- Ask the user to enter a temperature in Celsius. The program should print a message
+based on the temperature:
+(a) If the temperature is less than -273.15, print that the temperature is invalid because
+    it is below absolute zero.
+(b) If it is exactly -273.15, print that the temperature is absolute 0.
+(c) If the temperature is between-273.15 and 0,print that the temperature is below 
+    freezing.
+(d) If it is 0, print that the temperature is at the freezing point.
+(e) If it is between 0 and 100, print that the temperature is in the normal range.
+(f) If it is 100, print that the temperature is at the boiling point.
+(g) If it is above 100, print that the temperature is above the boiling point.
+'''
+temp2 = float(input("\nEnter temperature in Celsius: "))
 
+if temp2 < -273.15:
+    print("Temperature is invalid because it is below absolute zero.")
+elif temp2 == -273.15:
+    print("Temperature is absolute zero.")
+elif temp2 > -273.15 and temp2 < 0:
+    print("Temperature is below freezing.")
+elif temp2 == 0:
+    print("Temperature is at the freezing point.")
+elif temp2 > 0 and temp2 < 100:
+    print("Temperature is in the normal range.")
+elif temp2 == 100:
+    print("Temperature is at the boiling point.")
+else:
+    print("Temperature is above the boiling point.")
 
 
 
