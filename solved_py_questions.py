012330124d3dd056debbfea91c6d8dba0524e46d
worladev <confidenceworlasi@gmail.com>
2023-07-08 02:36:52 +0100

    solved 19th question- (part a)

--- solved_py_questions.py
+++ solved_py_questions.py
@@ -341,8 +341,26 @@ while count < 1:
 
 
 '''NO 19
-
+- Recall that, given a string s, s.index('x') returns the index of the first x in s
+and an error if there is no x.
+(a) Write a program that asks the user for a string and a letter. Using a while loop,
+the program should print the index of the first occurrence of that letter and a message
+if the string does not contain the letter.
+(b) Write the above program using a for/break loop instead of a while loop.
 '''
+
+# Using while loop
+str = input("\nEnter any string: ")
+letter = input("Enter a letter of the alphabet.: ")
+
+check = True
+while check:
+    if letter in str:
+        print(str.index(letter))
+        check = False
+    else:
+        print(f"{letter} not in {str}.")
+        break
 
 
 
