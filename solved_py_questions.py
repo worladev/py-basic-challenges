1ab336cc4e12f5723fe79545af8781d9ef192e5c
worladev <confidenceworlasi@gmail.com>
2023-07-16 01:52:57 +0100

    solved 33rd question (a & b)

--- solved_py_questions.py
+++ solved_py_questions.py
@@ -785,3 +785,34 @@ for key in data:
     print(f"{key} has no email")
 
 
+
+''''NO 33
+days = {
+    'January':31, 'February':28, 'March':31,'April':30, 'May':31, 'June':30,
+    'July':31, 'August':31, 'September':30, 'October':31, 'November':30, 'December':31
+    }
+(a) Ask the user to enter a month name and use the dictionary to tell
+them how many days are in the month.
+(b) Print out all of the keys in alphabetical order.
+(c) Print out all of the months with 31 days.
+(d) Print out the (key-value) pairs sorted by the number of days in each month
+
+'''
+
+days = {
+    'January':31, 'February':28, 'March':31,'April':30, 'May':31, 'June':30,
+    'July':31, 'August':31, 'September':30, 'October':31, 'November':30, 'December':31
+    }
+
+# (a) Ask the user to enter a month name and use the dictionary to tell
+    # them how many days are in the month.
+month_name = input("Enter a month name: ").capitalize()
+days_in_month = days[month_name]
+print(f"There are {days_in_month} days in {month_name}")
+
+# (b) Print out all of the keys in alphabetical order.
+order = [key for key, _ in days.items()]
+order.sort()
+print(order)
+
+
