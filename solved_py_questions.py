524bca153d23f99e9b0d2571d52260f43228449e
worladev <confidenceworlasi@gmail.com>
2023-07-22 17:01:02 +0000

    solved 40th question

--- solved_py_questions.py
+++ solved_py_questions.py
@@ -1058,6 +1058,23 @@ double_li.insert(3, 25)
 print("\n", double_li)
 
 
-'''NO 40
-
 '''
+NO 40
+- Write a program that takes any two lists L and M of the same size and adds their elements
+together to form a new list N whose elements are sums of the corresponding elements in L and M.
+For instance, if L=[3,1,4] and M=[1,5,9], then N should equal [4,6,13].
+'''
+
+def add_list(l, m):
+    n = list()
+    if len(l) == len(m):
+        for i in range(len(l)):
+            n.append(l[i] + m[i])
+    return n
+
+l1 = [3,1,4]
+l2 = [1,5,9]
+
+print(add_list(l1, l2))
+
+
