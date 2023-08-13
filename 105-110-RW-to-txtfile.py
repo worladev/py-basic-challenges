18c3c04d2f928a9189e642061b04872312d63f9f
worladev <confidenceworlasi@gmail.com>
2023-08-13 20:38:07 +0000

    solved challenge 105-110 on reading and writing to a file

--- 105-110-RW-to-txtfile.py
+++ 105-110-RW-to-txtfile.py
@@ -36,3 +36,80 @@ with open('txtFiles/Names.txt', mode='r') as challenge_file:
     print(file3)
 
 
+'''No 108
+Open the Names.txt file. Ask the user to input a new name. Add this to the end of the file and display
+the entire file. 
+'''
+new_name = input('Enter a new name: ')
+with open('txtFiles/Names.txt', mode='a') as challenge_file:
+    file4 = challenge_file.write(new_name)
+
+with open('txtFiles/Names.txt', mode='r') as challenge_file:
+    file4 = challenge_file.read()
+    print(file4)
+
+
+'''No 109
+Display the following menu to the user:
+    1) Create a new file
+    2) Display the file
+    3) Add anew item to the file
+    Make a selection 1, 2 or 3:
+
+Ask the user to enter 1, 2 or 3. If they select anything other than 1, 2 or 3 it should display a
+suitable error message.
+
+If they select 1, ask the user to enter a school subject and save it to a new
+file called “Subject.txt”.
+It should overwrite any existing file with a new file.
+
+If they select 2, display the contents of the “Subject.txt” file.
+
+If they select 3, ask the user to enter a new subject and save it to the file and then display
+the entire contents of the file. Run the program several times to test the options.
+'''
+print('''
+1) Create a new file
+2) Display the file
+3) Add a new item to the file''')
+choice = int(input("\nMake a selection 1, 2 or 3: "))
+
+if choice == 1:
+    subject = input("Enter a school subject: ")
+    with open('txtFiles/Subject.txt', mode='w') as challenge_file:
+        file5 = challenge_file.write(subject)
+elif choice == 2:
+    with open('txtFiles/Subject.txt', mode='r') as challenge_file:
+        file5 = challenge_file.read()
+        print(file5)
+elif choice == 3:
+    new_subject = input("Enter a school subject: ")
+    with open('txtFiles/Subject.txt', mode='a') as challenge_file:
+        file5 = challenge_file.write(f"{new_subject}\n")
+    
+    with open('txtFiles/Subject.txt', mode='r') as challenge_file:
+        file5 = challenge_file.read()
+        print(file5)
+else:
+    print("Invalid choice.")
+
+
+'''No 110
+Using the Names.txt file you created earlier, display the list of names in Python. Ask the user
+to type in one of the names and then save all the names except the one they entered into a new
+file called Names2.txt. 
+'''
+file = open('txtFiles/Names.txt', mode='r')
+print(file.read())
+file.close()
+
+file = open('txtFiles/Names.txt', mode='r')
+select = input('Enter a name from the list: ')
+select = select + '\n'
+for row in file:
+    if row != select:
+        file = open('txtFiles/Names2.txt', mode='a')
+        new_record = row
+        file.write(new_record)
+        file.close()
+file.close()
