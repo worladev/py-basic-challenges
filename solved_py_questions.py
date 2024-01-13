c72f6151b75119cd97fc95898efeb457f32b8196
worladev <confidenceworlasi@gmail.com>
2024-01-13 21:34:52 +0000

    problem statement to challenge number 47

--- solved_py_questions.py
+++ solved_py_questions.py
@@ -1355,3 +1355,17 @@ word3 = "clean"
 result3 = find_first_duplicate(word3)
 print(result3)  # Output: 'None'
 
+
+'''NO 47
+DATA STRUCTURE AND ALGORITHMS
+TITLE: Array Quadruplet
+Given an unsorted array of integers arr and a number s, write a function findArrayQuadruplet that finds four
+numbers (quadruplet) in arr that sum up to s. Your function should return an array of these numbers in an
+ascending order. If such a quadruplet doesn't exist, return an empty array.
+
+Note that there may be more than one quadruplet in arr whose sum is s. You're asked to return the first one
+you encounter (considering the results are sorted).
+
+Input:  arr = [2, 7, 4, 0, 9, 5, 1, 3], s = 20
+Expected Output: [0, 4, 7, 9] 
+'''
