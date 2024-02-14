ae2de971eea7467c72a7235e5887f9b7dad31e90
worladev <confidenceworlasi@gmail.com>
2024-02-14 21:07:34 +0000

    problem statement to first hackerrank challenge

---
+++ solved-py-questions2.py
@@ -0,0 +1,19 @@
+''' HACKERRANK PYTHON PROBLEMS '''
+
+'''
+PROBLEM STATEMENT 1: Given the participants' score sheet for your University Sports Day, you are required to
+find the runner-up score. You are given n scores. Store them in a list and find the score of the runner-up.
+
+INPUT FORMAT: The first line contains . The second line contains an array A[] of n integers each separated by
+a space.
+
+CONSTRAINTS:
+    2 <= n <= 10
+    -100 <= A[i] <= 100
+
+OUTPUT FORMAT: Print the runner-up score.
+
+SAMPLE INPUT
+5
+2 3 6 6 5
+'''
